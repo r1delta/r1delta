d006b7d459904104c062b883bff747174cce870f
dr3murr <r3muxd@gmail.com>
2025-04-15 19:12:23 -0400

    grenade fixup

--- masterserver.cpp
+++ masterserver.cpp
@@ -110,7 +110,7 @@ namespace MasterServerClient {
                 hasWarned = true;
                 Warning("hide_server is 1, ignoring master server heartbeat requests\n");
             }
-            return true;
+            //return true;
         }
 
         std::lock_guard<std::mutex> lock(httpMutex);
