de28a74026901f9337e32b791f48a698461b9b47
dr3murr <r3muxd@gmail.com>
2025-04-24 12:48:29 -0400

    feat: Add delta_autoBalanceTeams convar

--- load.cpp
+++ load.cpp
@@ -2495,6 +2495,7 @@ do_server(const LDR_DLL_NOTIFICATION_DATA* notification_data)
 	RegisterConVar("hide_server", "0", FCVAR_NONE, "Whether the server should be hidden from the master server");
 	RegisterConVar("server_description", "", FCVAR_NONE, "Server description");
 	RegisterConVar("delta_ui_server_filter", "0", FCVAR_NONE, "Script managed vgui filter convar");
+	RegisterConVar("delta_autoBalanceTeams", "1", FCVAR_NONE, "Whether to autobalance teams on death/private match/lobby start. Managed by script");
 	CBanSystem::m_pSvBanlistAutosave = RegisterConVar("sv_banlist_autosave", "1", FCVAR_ARCHIVE, "Automatically save ban lists after modification commands.");
 	RegisterConCommand("script", script_cmd, "Execute Squirrel code in server context", FCVAR_GAMEDLL | FCVAR_CHEAT);
 	if (!IsDedicatedServer()) {
