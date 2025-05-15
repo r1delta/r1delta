49100adbf586c6e397d5aee000d73678d6086741
Alexandr German <mrsteyk1@vk.com>
2025-05-15 16:48:01 +0500

    remove mimalloc.
    Misc: fix debug assert on isprint for me and partial ASan work (I didn't stage all the fixes for vstdlib loading early) and ASan assert fixes.
    TODO(mrsteyk): make a memory logging function?

--- memory.cpp
+++ memory.cpp
@@ -42,12 +42,19 @@
 #pragma intrinsic(_ReturnAddress)
 
 
-IMemAlloc* g_pMemAllocSingleton;
+IMemAlloc* g_pMemAllocSingleton = 0;
 extern "C" __declspec(dllexport) IMemAlloc * CreateGlobalMemAlloc() {
-	if (!g_pMemAllocSingleton) {
-        g_pMemAllocSingleton = new CMimMemAlloc();
-	}
-	return g_pMemAllocSingleton;
+    if (!g_pMemAllocSingleton) {
+        // NOTE(mrsteyk): if to move it out - it will break due to static initialisation order.
+        static CMimMemAlloc mem;
+        g_pMemAllocSingleton = &mem;
+    }
+    return g_pMemAllocSingleton;
+}
+
+CMimMemAlloc* GlobalAllocator()
+{
+    return (CMimMemAlloc*)CreateGlobalMemAlloc();
 }
 
 void* __cdecl hkcalloc_base(size_t Count, size_t Size)
